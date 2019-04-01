f73ecf577bdfd7dfb0f3100d1fc04a7db2a79a3d
Mathew Kleppin <matkle414@gmail.com>
2019-04-01 13:17:25 -0400

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,55 +1,89 @@
-import React, { useState } from 'react'
-import useLocalForage from '../hooks/useLocalForage'
+import React, { useRef } from 'react'
 import './Counter.css'
 
 export default function Counter() {
-  const [count, setCount] = useLocalForage('count', 0)
-  const [changeCount, setChangeCount] = useState(1)
-  const [changeMin, setChangeMin] = useState(0)
-  const [changeMax, setChangeMax] = useState(100)
+  const ref = useRef();
+  const stream = new MediaStream();
+      
+  const peer = new RTCPeerConnection({
+    iceServers: [{
+      urls: [
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        ],
+    }],
+  })
+  peer.ontrack = (ev) => {
+    console.log(ev, 
+      ev.track.getSettings(),
+      ev.track.getCapabilities(),
+      ev.track.getConstraints(),
+    );
 
-  const requestChangeCount = (updatedCount) => {
-    if (updatedCount > changeMax) {
-      return;
-    } else if (updatedCount < changeMin) {
-      return;
+    const track = ev.track;
+    const constraints = {
+      // width: {min: 640, ideal: 1280},
+      // height: {min: 480, ideal: 720},
+      // advanced: [
+      //   {width: 1920, height: 1280},
+      //   {aspectRatio: 1.333}
+      // ]
+      
+    };
+
+    track.applyConstraints(constraints)
+
+    stream.addTrack(track);
+    const element = ref.current;
+    try {
+      element.srcObject = stream;
+    } catch (error) {
+      element.src = URL.createObjectURL(stream)
     }
-    setCount(updatedCount)
+
+    setTimeout(() => {
+      const settings = stream.getVideoTracks()[0].getSettings();
+      console.log(settings);
+    }, 10000)
+
+    // console.log(stream, peer);
+    // const settings = stream.getVideoTracks()[0].getSettings();
+    // console.log(settings);
   }
+  peer.onicecandidate = async (ev) => {
+    if (ev.candidate === null) {
+      const response = await fetch(
+        `https://gunk.partiallystapled.com/sdp/gxti`,
+        {
+          method: 'POST',
+          // mode: 'no-cors',                                                     
+          body: JSON.stringify(
+            peer.localDescription
+          ),
+        }
+      );
+      const data = await response.json();
+      peer.setRemoteDescription(new RTCSessionDescription(data))
+    }
+  }
+  peer
+    .createOffer({offerToReceiveVideo: true, offerToReceiveAudio: true})
+    .then(d => peer.setLocalDescription(d));
 
   return (
     <div className="counter">
-      <p>{count}</p>
-      <button 
-        className="button-primary" 
-        onClick={() => requestChangeCount(count + changeCount)}
-      >Add +</button>
-      <button 
-        className="button-primary" 
-        onClick={() => requestChangeCount(0)}
-      >Reset</button>
-      <button 
-        className="button-primary" 
-        onClick={() => requestChangeCount(count - changeCount)}
-      >Subtract -</button>
-
-      <div className="counter-options">
-        <input 
-          type="text"
-          value={changeMin}
-          onChange={({ target: { value } }) => setChangeMin(Number(value))} 
-        />
-        <input 
-          type="text"
-          value={changeCount}
-          onChange={({ target: { value } }) => setChangeCount(Number(value))} 
-        />
-        <input 
-          type="text"
-          value={changeMax}
-          onChange={({ target: { value } }) => setChangeMax(Number(value))} 
-        />
-      </div>
+      <video
+        ref={ref}
+        autoPlay
+        muted
+        controls
+        style={
+          {
+            width: '900px',
+            height: '600px',
+          }
+        }
+      />
     </div>
   )
 }
